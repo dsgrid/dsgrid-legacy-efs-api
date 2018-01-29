c7579e2d52bd8fd0413146e6a66075f66210c36f
Elaine Hale <elaine.hale@nrel.gov>
2018-01-29 16:46:06 -0700

    Working datafile and sectordataset tests.

--- tests/test_datafile.py
+++ tests/test_datafile.py
@@ -1,10 +1,12 @@
-from .temphdf5 import TempHDF5Filepath
+import os
 from py.test import raises
 from dsgrid.dataformat.datafile import Datafile
 from dsgrid.dataformat.sectordataset import SectorDataset
 from dsgrid.dataformat.enumeration import (
-    sectors_subsectors, counties, enduses, hourly2012
+    sectors_subsectors, counties, enduses, hourly2012,
+    enumdata_folder, MultiFuelEndUseEnumeration
 )
+from .temphdf5 import TempHDF5Filepath
 
 # Python2 doesn't have a FileNotFoundError
 try:
@@ -12,15 +14,19 @@ try:
 except NameError:
     FileNotFoundError = IOError
 
+def verify_file_not_there(filepath):
+    try:
+        raises(FileNotFoundError, Datafile.load, filepath)
+    except:
+        # monkeycup with Anaconda 3 throws OSError instead
+        raises(OSError, Datafile.load, filepath)
+
+
 def test_datafile_io():
 
     with TempHDF5Filepath() as filepath:
 
-        try:
-            raises(FileNotFoundError, Datafile.load, filepath)
-        except:
-            # monkeycup with Anaconda 3 throws OSError instead
-            raises(OSError, Datafile.load, filepath)
+        verify_file_not_there(filepath)
 
         datafile = Datafile(filepath,sectors_subsectors,counties,enduses,hourly2012)
         sector = datafile.add_sector("res__SingleFamilyDetached")
@@ -30,3 +36,25 @@ def test_datafile_io():
         datafile2 = Datafile.load(filepath)
 
         assert(datafile == datafile2)
+
+
+def test_datafile_io_fancy_enduses():
+    with TempHDF5Filepath() as filepath:
+
+        verify_file_not_there(filepath)
+
+        comstock_enduses = MultiFuelEndUseEnumeration.read_csv(
+            os.path.join(enumdata_folder,'comstock_enduses.csv'),
+            'ComStock Enduses')
+
+        datafile = Datafile(filepath,sectors_subsectors,counties,comstock_enduses,hourly2012)
+        subset_enduses = []
+        for i, _id in enumerate(comstock_enduses.ids):
+            if _id[0].startswith('facility'):
+                subset_enduses.append(_id)
+        sector = datafile.add_sector("com__Hotel",enduses=subset_enduses)
+        assert(sector is datafile["com__Hotel"])
+
+        datafile2 = Datafile.load(filepath)
+        assert(datafile == datafile2)
+
