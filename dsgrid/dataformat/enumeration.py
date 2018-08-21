268195bbc8e7da27ac25272d3d95c89ad4eb1b93
Elaine Hale <elaine.hale@nrel.gov>
2018-08-21 17:28:46 -0600

    New TimeEnumeration functionality to convert between itself, datetime, pytz, pandas. Also encoding what we mean by EST, PST, etc.

--- dsgrid/dataformat/enumeration.py
+++ dsgrid/dataformat/enumeration.py
@@ -1,5 +1,9 @@
+import datetime as dt
+from enum import Enum
 import os
 import logging
+import pytz
+import re
 
 import numpy as np
 import pandas as pd
@@ -173,6 +177,211 @@ class EndUseEnumerationBase(Enumeration):
 
 class TimeEnumeration(Enumeration):
     dimension = "time"
+
+    TIMESTAMP_POSITION = Enum('TIMESTAMP_POSITION',
+                              ['period_beginning',
+                               'period_midpoint',
+                               'period_ending'])
+
+    TIMEZONE_DISPLAY_NAMES = {
+        'Etc/GMT+5': 'EST',
+        'Etc/GMT+6': 'CST',
+        'Etc/GMT+7': 'MST',
+        'Etc/GMT+8': 'PST' }
+
+    TIMEZONE_LOOKUP = {val: key for key, val in TIMEZONE_DISPLAY_NAMES.items()}
+
+    @classmethod
+    def create(cls,enum_name,start,duration,resolution,
+               extent_timezone=pytz.timezone('UTC'),
+               store_timezone=None,
+               timestamp_position=TIMESTAMP_POSITION['period_ending']):
+        """
+        Create a new time enumeration based on the specified temporal extents,
+        resolution, and timezone.
+
+        Parameters
+        ----------
+        enum_name : str
+            name for this enumeration, ideally descriptive of the parameters
+            used for creation
+        start : datetime.datetime
+            beginning of the time period to be represented by the timestamps
+        duration : datetime.timedelta
+            total length of time to be covered
+        resolution : datetime.timedelta
+            timestep for the enumeration
+        extent_timezone : pytz.timezone
+            timezone that should be used to interpret the extent parameters
+        store_timezone : None or pytz.timezone
+            timezone to write the ids and names in. If None, extent_timezone is
+            used.
+        timestamp_position : TimeEnumeration.TIMESTAMP_POSITION or convertable str
+            whether timestamps are placed at the beginning, ending, or midpoint 
+            of the time period being described
+
+        Returns
+        -------
+        TimeEnumeration
+        """
+        num_steps = duration / resolution
+        if not (num_steps == int(num_steps)):
+            logger.warn("Duration {} is not divided cleanly into steps of size {}".format(duration,resolution))
+        
+        extent_timezone = cls._timezone_object(extent_timezone)
+        store_timezone = cls._timezone_object(store_timezone,extent_timezone)
+
+        end = start + duration
+        ts_pos = timestamp_position if isinstance(timestamp_position,cls.TIMESTAMP_POSITION) else cls.TIMESTAMP_POSITION[timestamp_position]
+        
+        next_stamp = start
+        if ts_pos == cls.TIMESTAMP_POSITION['period_ending']:
+            next_stamp = start + resolution
+        elif ts_pos == cls.TIMESTAMP_POSITION['period_midpoint']:
+            next_stamp = start + (resolution / 2)
+
+        ids = []
+        while next_stamp <= end:
+            ids.append(str(extent_timezone.localize(next_stamp).astimezone(store_timezone)))
+            next_stamp = next_stamp + resolution
+
+        return cls(enum_name,ids,ids)
+
+    @classmethod
+    def _timezone_object(cls,timezone,default=None):
+        result = timezone
+        if timezone is None:
+            result = default
+        if result in cls.TIMEZONE_LOOKUP:
+            result = cls.TIMEZONE_LOOKUP[result]
+        if isinstance(result,str):
+            result = pytz.timezone(result)
+        return result
+
+    @property
+    def store_timezone(self):
+        """
+        Examines the first id to determine what timezone this TimeEnumeration
+        is stored in. Assumes the usage of datetime, pytz, and the "standard" 
+        timezones, e.g., 
+        
+            - pytz.timezone('Etc/GMT+5') = EST
+            - pytz.timezone('Etc/GMT+6') = CST
+            - pytz.timezone('Etc/GMT+7') = MST
+            - pytz.timezone('Etc/GMT+8') = PST
+        """
+        if not self.ids:
+            raise DSGridValueError('No instances in this {}. Cannot determine a timezone.'.format(type(self)))
+        m = re.match(r'[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}([+-][0-9]{2}:[0-9]{2})?',self.ids[0])
+        if not m:
+            raise DSGridValueError('Not able to interpret {} as a timestamp'.format(self.ids[0]))
+        if m.group(1) is None:
+            logger.warn('Explicit timezone not found in timestamp {}, assuming UTC'.format(self.ids[0]))
+            return pytz.timezone('UTC')
+        assert m.group(1)[3:] == ':00', m.group(1)
+        tz_str = 'Etc/GMT'
+        tz_str += '+' if m.group(1)[0] == '-' else '-'
+        tz_str += str(int(m.group(1)[1:3]))
+        return pytz.timezone(tz_str)
+
+    @property
+    def store_timezone_display_name(self):
+        """
+        Interprets self.ids[0] to report what timezone this enumeration is 
+        stored in. Converts from pytz strings to what we typically use, namely
+        EST, CST, MST, or PST.
+
+        Returns
+        -------
+        str
+            timezone this TimeEnumeration is stored in, per self.store_timezone 
+            and self.TIMEZONE_DISPLAY_NAMES
+        """
+        result = str(self.store_timezone)
+        if result in self.TIMEZONE_DISPLAY_NAMES:
+            result = self.TIMEZONE_DISPLAY_NAMES[result]
+        return result
+
+    @property
+    def resolution(self):
+        """
+        The resolution of this TimeEnumeration.
+
+        Returns
+        -------
+        dt.timedelta or array of dt.timedelta
+            Returns a single value if the intervals are all of the same length.
+            Returns a vector of values if they are different.
+        """
+        ind = self.to_datetime_index()
+        result = (ind[1:] - ind[:-1])
+        unique_vals = result.unique()
+        if len(unique_vals) == 1:
+            return unique_vals.to_pytimedelta()[0]
+        return result.to_pytimedelta()
+
+    def get_extents(self,report_timezone=None,
+                    timestamp_position=TIMESTAMP_POSITION['period_ending']):
+        """
+        Returns the inclusive temporal extents represented in this 
+        TimeEnumeration. That interpretation requires knowledge of the 
+        timestamp_postion--beginning, end, or midpoint of the period being 
+        described.
+
+        Parameters
+        ----------
+        report_timezone : pytz.timezone
+            Timezone in which to report out the result
+
+        Returns
+        -------
+        (datetime.datetime,datetime.datetime)
+            Tuple of start and end times, inclusive of all time represented 
+            based on the timestamp position, and in report_timezone. 
+        """
+        ind = self.to_datetime_index(return_timezone=report_timezone)
+        res = self.resolution
+        bres = res; eres = res
+        if not isinstance(res,dt.timedelta):
+            logger.warn("Temporal resolution is not uniform. Reported extents may be inaccurate.")
+            bres = res[0]; eres = res[-1]
+        start = ind[0].to_pydatetime(); end = ind[-1].to_pydatetime()
+
+        ts_pos = timestamp_position if isinstance(timestamp_position,self.TIMESTAMP_POSITION) else self.TIMESTAMP_POSITION[timestamp_position]
+        if ts_pos == self.TIMESTAMP_POSITION['period_beginning']:
+            end = end + eres
+        elif ts_pos == self.TIMESTAMP_POSITION['period_midpoint']:
+            start = start - (bres / 2)
+            end = end + (eres / 2)
+        elif ts_pos == self.TIMESTAMP_POSITION['period_ending']:
+            start = start - bres
+
+        return (start, end)        
+
+    def to_datetime_index(self,return_timezone=None):
+        """
+        Return a Pandas DatetimeIndex corresponding to this TimeEnumeration. 
+        By default, localizes the timestamps to the timezone inferred based on 
+        the text of the first enumeration id. If return_timezpone is None, this 
+        is what is returned. If return_timezone is not None, the index is 
+        converted to that timezone before being returned.
+
+        Parameters
+        ----------
+        return_timezone : None or pytz.timezone
+            timezone of the returned index. If None, this is inferred from 
+            self.ids[0]
+        
+        Returns
+        -------
+        pandas.DatetimeIndex
+            same length as self.ids, but strings are converted to 
+            datetime.datetime objects and localized to a timezone.
+        """
+        df = pd.DataFrame([],index=self.ids)
+        return_timezone = self._timezone_object(return_timezone,self.store_timezone)
+        df.index = pd.to_datetime(df.index).tz_localize('UTC').tz_convert(return_timezone)
+        return df.index
 
 
 # Define data units -- these are ultimately associated with end-uses
